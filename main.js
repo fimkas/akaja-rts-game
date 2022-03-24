1a6ec539f1eea30a5501f44bf699a2bf0aa04e4f
fimkas <makxb2004@gmail.com>
2022-03-24 16:31:26 +0300

    2

--- main.js
+++ main.js
@@ -51,16 +51,16 @@ const resource = {
 document.addEventListener('keydown', (event) => {
     const keyName = event.code;
 
-    if (keyName === 'KeyW') {
+    if (keyName === 'KeyW') { //вверх
         window.scrollBy(0, -15)
     }
-    if (keyName === 'KeyS') {
+    if (keyName === 'KeyS') { //вниз
         window.scrollBy(0, 15)
     }
-    if (keyName === 'KeyA') {
+    if (keyName === 'KeyA') { //влево
         window.scrollBy(-15, 0)
     }
-    if (keyName === 'KeyD') {
+    if (keyName === 'KeyD') { //вправо
         window.scrollBy(15, 0)
     }
 
@@ -72,34 +72,33 @@ function gold_f() {
         resource.gold_mine_production = i * 3
     }
     resource.gold += resource.gold_mine_production //золото от шахт
-
     resource.gold += resource.tax //золото от налогов
 
     resource.gold_production = resource.gold_mine_production + resource.tax; //сумирование всех источников дохода
 
+    resource.gold -= resource.gold_use // вычитаем потребление золота из прибили
     //расчет прибыли
-    resource.gold -= resource.gold_use
-    resource.gold_profit = resource.gold_production - resource.gold_use
+    resource.gold_profit = resource.gold_production - resource.gold_use //получаем чистую доходность за ход
 }
 function food_f() {
     for (let i = 1; i <= resource.farm; i++) {
         resource.farm_production = i * 3
     }
     resource.food += resource.farm_production //еда от ферм
-
+    resource.food -= resource.food_use // вычитаем потребление еды из прибили
     //расчет прибыли
-    resource.food -= resource.food_use
-    resource.farm_profit = resource.farm_production - resource.food_use
+    resource.farm_profit = resource.farm_production - resource.food_use //получаем чистую доходность за ход
 
 }
 function wood_f() {
+    //считаем сколь приносит одна лесопилка
     for (let i = 1; i <= resource.sawmill; i++) {
         resource.sawmill_production = i * 3
     }
-    resource.wood += resource.sawmill_production
-
-    resource.wood -= resource.wood_use
-    resource.wood_profit = resource.sawmill_production - resource.wood_use
+    resource.wood += resource.sawmill_production //общая доходность всех лесопилок
+    resource.wood -= resource.wood_use //вычитаем расход дерева из прибилии
+    //расчет прибыли
+    resource.wood_profit = resource.sawmill_production - resource.wood_use //получаем чистый доход за один ход
 }
 function stone_f() {
     for (let i = 1; i <= resource.stone_mine; i++) {
@@ -112,40 +111,47 @@ function stone_f() {
 }
 
 // function people_f() {
-//     for (let i = 1; i <= resource.stone_mine; i++) {
-//         resource.stone_mine_production = i * 3
-//     }
 // }
 
 function resource_update() {
-    document.querySelector('#resource_gold').innerHTML = "gold: " + resource.gold + " " + resource.gold_profit
-    document.querySelector('#resource_food').innerHTML = "food: " + resource.food  + " " + resource.farm_profit
-    document.querySelector('#resource_wood').innerHTML = "wood: " + resource.wood + " " + resource.wood_profit
-    document.querySelector('#resource_stone').innerHTML = "stone: " + resource.stone + " " + resource.stone_profit
-    document.querySelector('#resource_people').innerHTML = "people: " + resource.people_use + "/" + resource.people_limit
+    //вызов функций ресурсов
+    gold_f(); food_f(); wood_f(); stone_f()
+    //золото
+    document.querySelector('#resource_gold').innerHTML =
+        "gold: " + resource.gold + " " + resource.gold_profit
+    //еда
+    document.querySelector('#resource_food').innerHTML =
+        "food: " + resource.food  + " " + resource.farm_profit
+    //дерево
+    document.querySelector('#resource_wood').innerHTML =
+        "wood: " + resource.wood + " " + resource.wood_profit
+    //камень
+    document.querySelector('#resource_stone').innerHTML =
+        "stone: " + resource.stone + " " + resource.stone_profit
+    //люди
+    document.querySelector('#resource_people').innerHTML =
+        "people: " + resource.people_use + "/" + resource.people_limit
 }
 
 //генерация мира:
-for (let i = 1; i <= system.cell_lend; i++) { //создание плиток
+for (let i = String(1); i <= system.cell_lend; i++) { //создание плиток
     let div = document.createElement('div')
     div.className = "cell"
-    div.innerHTML = ".";
     div.id = i
     document.querySelector('#cells').append(div)
 }
 
 for (let i = 1; i <= system.hills + Math.floor(Math.random() * 3); i++) { // создание гор в рандомном месте
-    const random = Math.floor(Math.random() * system.cell_lend)
+    const random = String(Math.floor(Math.random() * system.cell_lend))
     let rand_hills = Math.floor(Math.random() * 3)
+    //выдача класов
     document.getElementById(random).classList.add('hills' + rand_hills);
     document.getElementById(random).classList.add('hills');
-
-    document.getElementById(random).innerHTML = "hills"
 }
 
 for (let i = 1; i <= system.gold_ore + Math.floor(Math.random() * 3); i++) { // создание золотых жыл в рандомном месте
-    let random = Math.floor(Math.random() * system.cell_lend)
-    document.getElementById(random).innerHTML = "gold_ore"
+    let random = String(Math.floor(Math.random() * system.cell_lend))
+    //выдача класов
     document.getElementById(random).classList.add('gold_ore');
 }
 
@@ -153,34 +159,26 @@ for (let i = 1; i <= system.gold_ore + Math.floor(Math.random() * 3); i++) { // 
 document.querySelector('#town-hall__menu-button').onclick = function () {
     system.build_it = "town_hall"
 }
-
 document.querySelector('#farm__menu-button').onclick = function () {
     system.build_it = "farm"
 }
-
 document.querySelector('#sawmill__menu-button').onclick = function () {
     system.build_it = "sawmill"
 }
-
 document.querySelector('#mine__menu-button').onclick = function () {
     system.build_it = "mine"
 }
-
 document.querySelector('#house__menu-button').onclick = function () {
     system.build_it = "house"
 }
 
 //кнопка хода
 document.querySelector('#turn__menu-button').onclick = function () {
-    if (system.town_hall === 0){
+    if (system.town_hall === 0){ //предупреждение
         alert("установите ратушу")
     }
-    if (system.town_hall !== 0){
+    if (system.town_hall !== 0){ // в случаее успеха закончить ход
         //вывод ресурсов на панель:
-        gold_f()
-        food_f()
-        wood_f()
-        stone_f()
         resource_update()
 
         // дебаг:
@@ -195,11 +193,10 @@ document.querySelector('#turn__menu-button').onclick = function () {
     }
 }
 // выполнение функции строительства:
-for (let i = 1; i <= system.cell_lend; i++) {
+for (let i = String(1); i <= system.cell_lend; i++) {
     document.getElementById(i).onclick = function (){
 
-        if (system.build_it === "town_hall" && document.getElementById(i).innerHTML === "." && system.town_hall !== 1) {
-            document.getElementById(i).innerHTML = system.build_it
+        if (system.build_it === "town_hall" && document.getElementById(i).className === "cell" && system.town_hall !== 1) {
             document.getElementById(i).classList.add('town_hall');
             document.querySelector('#town-hall__menu-button').style.display = 'none'
             system.town_hall = 1
@@ -210,8 +207,7 @@ for (let i = 1; i <= system.cell_lend; i++) {
             system.build_it = ""
         }
 
-        if (system.build_it === "farm" && document.getElementById(i).innerHTML === "." && system.town_hall !== 0) {
-            document.getElementById(i).innerHTML = system.build_it
+        if (system.build_it === "farm" && document.getElementById(i).className === "cell" && system.town_hall !== 0) {
             document.getElementById(i).classList.add('farm');
             resource.farm++
 
@@ -222,8 +218,7 @@ for (let i = 1; i <= system.cell_lend; i++) {
             system.build_it = ""
         }
 
-        if (system.build_it === "sawmill" && document.getElementById(i).innerHTML === "." && system.town_hall !== 0) {
-            document.getElementById(i).innerHTML = system.build_it
+        if (system.build_it === "sawmill" && document.getElementById(i).className === "cell" && system.town_hall !== 0) {
             resource.sawmill++
             resource.food_use++
 
@@ -231,8 +226,7 @@ for (let i = 1; i <= system.cell_lend; i++) {
             system.build_it = ""
         }
 
-        if (system.build_it === "mine" && document.getElementById(i).innerHTML === "gold_ore" && system.town_hall !== 0) {
-            document.getElementById(i).innerHTML = system.build_it + "gold"
+        if (system.build_it === "mine" && document.getElementById(i).className === "cell gold_ore" && system.town_hall !== 0) {
             resource.gold_mine++
             resource.food_use++
 
@@ -240,8 +234,12 @@ for (let i = 1; i <= system.cell_lend; i++) {
             system.build_it = ""
         }
 
-        if (system.build_it === "mine" && document.getElementById(i).innerHTML === "hills" && system.town_hall !== 0) {
-            document.getElementById(i).innerHTML = system.build_it + "stone"
+        if (system.build_it === "mine" &&
+            document.getElementById(i).className === "cell hills1 hills" ||
+            document.getElementById(i).className === "cell hills2 hills" ||
+            document.getElementById(i).className === "cell hills3 hills" &&
+            system.town_hall !== 0) {
+
             resource.stone_mine++
             resource.food_use++
 
@@ -255,17 +253,17 @@ for (let i = 1; i <= system.cell_lend; i++) {
                 document.getElementById(i).classList.add('mine2')
             }
 
-
             resource_update()
             system.build_it = ""
         }
 
-        if (system.build_it === "house" && document.getElementById(i).innerHTML === "." && system.town_hall !== 0){
-            document.getElementById(i).innerHTML = system.build_it
+        if (system.build_it === "house" && document.getElementById(i).className === "cell" && system.town_hall !== 0){
             resource.tax++
             resource.food_use++
             resource.people_limit += 10
 
+            resource_update()
+            system.build_it = ""
         }
 
     }
